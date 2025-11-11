2b6e9ea47c7f494090c5bda9b206643c7ed87c27
shubhamdarwante2301 <shubhamdarwante2301@gmail.com>
2025-11-11 17:57:37 +0530

    Update Home.js
    testing changes are reverted back.

--- src/components/Home.js
+++ src/components/Home.js
@@ -97,7 +97,7 @@ const Home = () => {
   return (
     <HomeStyle id="hero">
       <ProfileDetail>
-        <Heading>Hello! My name is</Heading>
+        <Heading>Hello! I'm</Heading>
         <Name>Shubham Darwante</Name>
         <Description>Front-End Developer</Description>
         <Socialicons />
